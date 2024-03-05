6f3390222c4562f6dbb24abe037ca65c6f73dc2b
ayelabilal <ayelabilal944@gmail.com>
2024-03-05 15:19:52 +0500

    commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// Array
+
+// 1
+var name = []
+console.log("name")
+// 2
+// var a = ["ayela"]
+// alert(a)
+
+// 3
+var name1 = ["ayela"]
+console.log(name1)
+
+// 4
+var number = ["2"]
+document.write(number)
+
+// 5
+var bolean = ["false"]
+console.log(bolean)
+
+// 6
+var mix = ["ayela","3","true"]
+console.log(mix)
+
+// 7
+var education = ["1)SSC" , "<br>","2)HSC","<br>","BCS","<br>","BS","<br>","B.COM","<br>","MS","<br>","M.Phil.","<br>","PhD"]
+document.write("<h4>Qualification</h4>","<br>", education)
+
+// 8
+// var Arra = ["Michael","John","Tony"]
+// var arr = ["320","230","480"]
+// var per = (Arra + arr + per)/500*100 
+// alert(per + "%")
+
+// 9 a
+var colors = ["red", "blue","green"]
+document.write ("<br>",colors)
+
+// b
+var color = ["red","green","blue","pink","black"]
+console.log(color.push("yellow"))
+console.log(color)
+
+// c
+var color = ["red","green","blue"]
+console.log(color.unshift("pink","purple"))
+console.log(color)
+
+// d
+var color = ["red","green","blue"]
+console.log(color.shift())
+console.log(color)
+
+// e
+var color = ["red","green","blue","pink","black"]
+console.log(color.pop())
+console.log(color)
+
+// f
+var color = ["pink","blue","orange","white","green"]
+console.log = (color)
