33a0f47281fa5566fb9123deb1d72ce639693c91
ayelabilal <ayelabilal944@gmail.com>
2024-03-05 17:59:25 +0500

    complete

--- app.js
+++ app.js
@@ -59,4 +59,68 @@ console.log(color)
 
 // f
 var color = ["pink","blue","orange","white","green"]
-console.log = (color)
+console.log(color.splice (2, 2, "purple"))
+console.log(color)
+
+//10
+var score = ["320","230","480","120"]
+console.log(score)
+score.sort()
+console.log(score)
+
+// 12
+var arr = ["This","is","my","cat"]
+console.log(arr)
+arr.join("");
+console.log(arr)
+
+           // chapter 15 (Array I)
+
+// 1
+var arr = [""]
+
+// 2
+var arr = [ "element"]
+
+// 3
+var alphabet = ["h","i","j","k","l","m","n","o"]
+alert("j")
+
+// 4
+var alphabet = ["h","i","j","k","l","m","n","o"]
+console.log(alphabet)
+
+          // chapter 16 (Array II)
+
+// 1
+var pets = ["cat","dog","cow"]
+console.log(pets.push("goat"))
+console.log(pets)
+
+// 2
+var alphabet = ["h","i","j","k",]
+console.log(alphabet.pop())
+console.log(alphabet)
+
+// 3
+var alphabet = ["h","i","j","k",]
+console.log(alphabet.push("22"))
+console.log(alphabet)
+
+                         // chapter 16 (Array III)
+
+// 1
+var size = ["S","M","xl","xxl","xxxl",]
+console.log(size.shift())
+console.log(size)
+
+// 2
+var size = ["S","M","xl","xxl","xxxl"]
+console.log(size.unshift("A","B","C"))
+console.log(size)
+
+// 4
+var sizes = ["S","M","xl","xxl","xxxl"]
+console.log(sizes.splice(1,2 ,"L"))
+console.log(sizes)
+
